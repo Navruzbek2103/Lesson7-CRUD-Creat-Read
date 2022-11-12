98e8abf8ad290f9b6cded40a3c8cc303cf475392
Navruzbek2103 <navrozbek2306@gmail.com>
2022-11-12 17:06:47 +0500

    Added edit function

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function renderData(data = []){
         <td>${item.id}</td>
         <td>${item.user_name}</td>
         <td>${item.age}</td>
-        <td><button class="btn btn-success" data-edit="${item.id}">Edit</button></td>
+        <td><button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" data-edit="${item.id}">Edit</button></td>
         <td><button class="btn btn-danger" data-delete="${item.id}">Delete</button></td>
       </tr>
       `
@@ -89,4 +89,52 @@ const deleteFunc = (id) => {
       body: JSON.stringify({})
     })
   }, 2000)
+}
+
+
+$("tbody").addEventListener("click", (p) => {
+  if(p.target.classList.contains("btn-success")){
+    let id = p.target.getAttribute("data-edit");
+    localStorage.setItem("editUser", id)
+    fetch(`http://localhost:2306/user/${id}`)
+    .then(resolve => resolve.json())
+    .then(result => setValue(result))
+    .catch(err => console.log(err))
+  }
+})
+
+const updateFunc = () => {
+  let id = localStorage.getItem("editUser")
+  let newUser = $("#userNameEdit").value.trim()
+  let newAge = $("#userAgeEdit").value.trim()
+
+  if(newUser.length === 0 || newAge.length === 0){
+    $(".toastify").innerHTML = "Empty is places"
+    $(".toastify").style.backgroundColor = 'crimson'
+    $(".toastify").style.color = 'white'
+    $(".toastify").style.textShadow = '0 0 5px white'
+    $(".toastify").style.transform = "translate(0)"
+  }
+
+  else{
+    fetch(`http://localhost:2306/user/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user_name: newUser,
+        age: newAge
+      })
+    })
+  }
+}
+
+$(".formEdit").addEventListener("submit", ()=>{
+  updateFunc()
+})
+
+function setValue(data){
+  $("#userNameEdit").value = data.user_name;
+  $("#userAgeEdit").value = data.age;
 }
