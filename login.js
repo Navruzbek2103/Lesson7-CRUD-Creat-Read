d40e92ab51c29d6458acdc960d093c761a73182a
Navruzbek2103 <navrozbek2306@gmail.com>
2022-11-15 17:11:54 +0500

    Added function

--- login.js
+++ login.js
@@ -2,18 +2,17 @@
 
 const BASE_URL = "https://n36-todolist.herokuapp.com"
 
-const registration = () => {
-  const user_name = $("#userName").value.trim();
-  const password = $("#userPassword").value.trim()
+const auth = () => {
+  const user_name = $("#user").value.trim();
+  const password = $("#password").value.trim()
 
   const parametres = {
-    userName: user_name,
-    userPassword: password
+    login: user_name,
+    password: password
   }
 
-  console.log(parametres);
 
-  fetch(`${BASE_URL}/signup`, {
+  fetch(`${BASE_URL}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,7 +20,6 @@ const registration = () => {
     body: JSON.stringify(parametres)
   }).then(resolve => resolve.json())
   .then(result => {
-
     if(result.message){
       $(".modal-toast").innerHTML = `<strong>${result.message}</strong>`;
       $(".modal-toast").classList.remove("hide-toast")
@@ -31,21 +29,23 @@ const registration = () => {
       }, 1000);
     }
     if(result.token){
+      localStorage.setItem("token", result.token)
+      localStorage.setItem("userName", parametres.login)
       $(".modal-toast").classList.remove('alert-danger');
       $(".modal-toast").classList.add('alert-success');
-      $(".modal-toast").innerHTML = `<strong>Registration successfully</strong>`;
+      $(".modal-toast").innerHTML = `<strong>Successfully LOGIN</strong>`;
       $(".modal-toast").classList.remove("hide-toast");
 
       setTimeout(() => {
-        $(".modal-toast").classList.add("hide-toast")
-        window.location.replace("./login.html")
+        $(".modal-toast").classList.add("hide-toast");
+        window.location.replace('./index.html')
       }, 2000);
 
     }
   })
 }
 
-$("#reg").addEventListener("submit", (e) => {
+$("#login").addEventListener("submit", (e) => {
   e.preventDefault();
-  registration()
+  auth()
 })
